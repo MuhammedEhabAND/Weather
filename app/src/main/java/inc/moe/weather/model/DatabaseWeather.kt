00c507f1f90a374b675265d642841ced23ef6325
MuhammedEhabAND <muhammedehab726@gmail.com>
2023-11-08 22:59:56 +0200

    fixes database creation bug

--- app/src/main/java/inc/moe/weather/model/DatabaseWeather.kt
+++ app/src/main/java/inc/moe/weather/model/DatabaseWeather.kt
@@ -4,51 +4,13 @@ import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
 @Entity(tableName = "weather", primaryKeys = ["timeZone", "isCached"])
-
 data class DatabaseWeather(
-    val long:Double,
-    val lat:Double,
-    @PrimaryKey
-    val timeZone:String,
-    val weatherType :String,
-    val temp:Double,
-    val image:String,
-    @PrimaryKey
-    val isCached:Boolean = false) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readDouble(),
-        parcel.readDouble(),
-        parcel.readString().toString(),
-        parcel.readString().toString(),
-        parcel.readDouble(),
-        parcel.readString().toString(),
-        parcel.readByte() != 0.toByte()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeDouble(long)
-        parcel.writeDouble(lat)
-        parcel.writeString(timeZone)
-        parcel.writeString(weatherType)
-        parcel.writeDouble(temp)
-        parcel.writeString(image)
-        parcel.writeByte(if (isCached) 1 else 0)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<DatabaseWeather> {
-        override fun createFromParcel(parcel: Parcel): DatabaseWeather {
-            return DatabaseWeather(parcel)
-        }
-
-        override fun newArray(size: Int): Array<DatabaseWeather?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    var long: Double=0.0,
+    var lat: Double=0.0,
+    var timeZone: String="",
+    var weatherType: String="",
+    var temp: Double=0.0,
+    var image: String="",
+    var isCached: Boolean = false
+)
